76d2b9587f2848990896c0af746fe7f3699ef16c
Harley McDowell <hmcdowell95@gmail.com>
2020-04-22 01:55:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,12 +22,7 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    a = @num1.to_i
-    @sum = 0 
-    a.times do
-      @sum += @num2
-    end
-    @sum
+    @num1 * @num2
   end
 
 end
